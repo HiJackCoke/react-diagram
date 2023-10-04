b923b6fb54322faf136e9c1b3a957a08e1be6b72
taehunlim <dsa1520@naver.com>
2023-10-04 16:57:01 +0900

    Fix: directory path error

--- vite.config.ts
+++ vite.config.ts
@@ -16,6 +16,7 @@ export default defineConfig({
          hooks: path.resolve(__dirname, 'src/hooks'),
          contexts: path.resolve(__dirname, 'src/contexts'),
          store: path.resolve(__dirname, 'src/store'),
+         fixtures: path.resolve(__dirname, 'src/fixtures'),
       },
    },
 
