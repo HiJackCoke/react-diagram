8ca5982481da707e0d30c84c2a40cf8553fab91a
taehunlim <dsa1520@naver.com>
2023-12-06 20:32:53 +0900

    Type: NodeDragItem

--- packages/src/hooks/useDrag/type.ts
+++ packages/src/hooks/useDrag/type.ts
@@ -1,22 +1,23 @@
 import { MouseEvent as ReactMouseEvent } from 'react';
 import { D3DragEvent, SubjectPosition } from 'd3';
 
-import { XYPosition, CoordinateExtent } from '../../types';
+import { XYPosition } from '../../types';
 import { Node } from '../../types';
 
 export type UseDragEvent = D3DragEvent<HTMLDivElement, null, SubjectPosition>;
 
-export type NodeDragItem = {
-   id: string;
-   position: XYPosition;
-   positionAbsolute: XYPosition;
-   // distance from the mouse cursor to the node when start dragging
+export type NodeDragItem = Pick<
+   Node,
+   | 'id'
+   | 'position'
+   | 'positionAbsolute'
+   | 'width'
+   | 'height'
+   | 'extent'
+   | 'parentNode'
+   | 'dragging'
+> & {
    distance: XYPosition;
-   width?: number | null;
-   height?: number | null;
-   extent?: 'parent' | CoordinateExtent;
-   parentNode?: string;
-   dragging?: boolean;
 };
 
 export type NodeDragHandler = (
